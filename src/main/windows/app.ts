8fc0133b3af2f9ae34f45e038a844c64edf65b59
Ruben Platiado <106348705+RubenPlatiado@users.noreply.github.com>
2024-08-21 13:47:48 -0700

    fit - devtools y escuchador de navegacion

--- src/main/windows/app.ts
+++ src/main/windows/app.ts
@@ -1,244 +1,247 @@
 /* Copyright (c) 2021-2024 Damon Smith */
 
-import { BrowserWindow, app, dialog } from 'electron';
-import { writeFileSync, promises } from 'fs';
-import { resolve, join } from 'path';
+import { BrowserWindow, app, dialog } from "electron"
+import { writeFileSync, promises } from "fs"
+import { resolve, join } from "path"
 
-import { getPath } from '~/utils';
-import { runMessagingService } from '../services';
-import { Application } from '../application';
-import { isNightly } from '..';
-import { ViewManager } from '../view-manager';
+import { getPath } from "~/utils"
+import { runMessagingService } from "../services"
+import { Application } from "../application"
+import { isNightly } from ".."
+import { ViewManager } from "../view-manager"
 
 export class AppWindow {
-  public win: BrowserWindow;
+    public win: BrowserWindow
 
-  public viewManager: ViewManager;
+    public viewManager: ViewManager
 
-  public incognito: boolean;
+    public incognito: boolean
 
-  public constructor(incognito: boolean) {
-    this.win = new BrowserWindow({
-      frame: false,
-      minWidth: 900,
-      minHeight: 250,
-      width: 900,
-      height: 700,
-      titleBarStyle: 'hiddenInset',
-      backgroundColor: '#ffffff',
-      webPreferences: {
-        plugins: true,
-        // TODO: enable sandbox, contextIsolation and disable nodeIntegration to improve security
-        nodeIntegration: true,
-        contextIsolation: false,
-        javascript: true,
-      },
-      trafficLightPosition: {
-        x: 18,
-        y: 18,
-      },
-      icon: resolve(
-        app.getAppPath(),
-        `static/${isNightly ? 'nightly-icons' : 'icons'}/icon.png`,
-      ),
-      show: false,
-    });
-    require('@electron/remote/main').enable(this.win.webContents);
-    this.incognito = incognito;
+    public constructor(incognito: boolean) {
+        console.log("HOLA MUNDO")
+        this.win = new BrowserWindow({
+            frame: false,
+            minWidth: 900,
+            minHeight: 250,
+            width: 900,
+            height: 700,
+            titleBarStyle: "hiddenInset",
+            backgroundColor: "#ffffff",
+            webPreferences: {
+                plugins: true,
+                // TODO: enable sandbox, contextIsolation and disable nodeIntegration to improve security
+                nodeIntegration: true,
+                contextIsolation: false,
+                javascript: true
+            },
+            trafficLightPosition: {
+                x: 18,
+                y: 18
+            },
+            icon: resolve(
+                app.getAppPath(),
+                `static/${isNightly ? "nightly-icons" : "icons"}/icon.png`
+            ),
+            show: false
+        })
+        require("@electron/remote/main").enable(this.win.webContents)
+        this.incognito = incognito
 
-    this.viewManager = new ViewManager(this, incognito);
+        this.viewManager = new ViewManager(this, incognito)
 
-    runMessagingService(this);
+        runMessagingService(this)
 
-    const windowDataPath = getPath('window-data.json');
+        const windowDataPath = getPath("window-data.json")
 
-    let windowState: any = {};
+        let windowState: any = {}
 
-    (async () => {
-      try {
-        // Read the last window state from file.
-        windowState = JSON.parse(
-          await promises.readFile(windowDataPath, 'utf8'),
-        );
-      } catch (e) {
-        await promises.writeFile(windowDataPath, JSON.stringify({}));
-      }
+        ;(async () => {
+            try {
+                // Read the last window state from file.
+                windowState = JSON.parse(
+                    await promises.readFile(windowDataPath, "utf8")
+                )
+            } catch (e) {
+                await promises.writeFile(windowDataPath, JSON.stringify({}))
+            }
 
-      // Merge bounds from the last window state to the current window options.
-      if (windowState) {
-        this.win.setBounds({ ...windowState.bounds });
-      }
+            // Merge bounds from the last window state to the current window options.
+            if (windowState) {
+                this.win.setBounds({ ...windowState.bounds })
+            }
 
-      if (windowState) {
-        if (windowState.maximized) {
-          this.win.maximize();
+            if (windowState) {
+                if (windowState.maximized) {
+                    this.win.maximize()
+                }
+                if (windowState.fullscreen) {
+                    this.win.setFullScreen(true)
+                }
+            }
+        })()
+
+        this.win.show()
+
+        // Update window bounds on resize and on move when window is not maximized.
+        this.win.on("resize", () => {
+            if (!this.win.isMaximized()) {
+                windowState.bounds = this.win.getBounds()
+            }
+        })
+
+        this.win.on("move", () => {
+            if (!this.win.isMaximized()) {
+                windowState.bounds = this.win.getBounds()
+            }
+        })
+
+        const resize = () => {
+            setTimeout(async () => {
+                if (process.platform === "linux") {
+                    await this.viewManager.select(
+                        this.viewManager.selectedId,
+                        false
+                    )
+                } else {
+                    await this.viewManager.fixBounds()
+                }
+            })
+
+            setTimeout(() => {
+                this.webContents.send("tabs-resize")
+            }, 500)
+
+            this.webContents.send("tabs-resize")
         }
-        if (windowState.fullscreen) {
-          this.win.setFullScreen(true);
-        }
-      }
-    })();
 
-    this.win.show();
+        this.win.on("maximize", resize)
+        this.win.on("restore", resize)
+        this.win.on("unmaximize", resize)
 
-    // Update window bounds on resize and on move when window is not maximized.
-    this.win.on('resize', () => {
-      if (!this.win.isMaximized()) {
-        windowState.bounds = this.win.getBounds();
-      }
-    });
+        this.win.on("close", async (event: Electron.Event) => {
+            const { object: settings } = Application.instance.settings
 
-    this.win.on('move', () => {
-      if (!this.win.isMaximized()) {
-        windowState.bounds = this.win.getBounds();
-      }
-    });
+            if (settings.warnOnQuit && this.viewManager.views.size > 1) {
+                const answer = dialog.showMessageBoxSync(null, {
+                    type: "question",
+                    title: `Quit ${app.name}?`,
+                    message: `Quit ${app.name}?`,
+                    detail: `You have ${this.viewManager.views.size} tabs open.`,
+                    buttons: ["Close", "Cancel"]
+                })
 
-    const resize = () => {
-      setTimeout(async () => {
-        if (process.platform === 'linux') {
-          await this.viewManager.select(this.viewManager.selectedId, false);
-        } else {
-          await this.viewManager.fixBounds();
-        }
-      });
+                if (answer === 1) {
+                    event.preventDefault()
+                    return
+                }
+            }
 
-      setTimeout(() => {
-        this.webContents.send('tabs-resize');
-      }, 500);
+            // Save current window state to a file.
+            windowState.maximized = this.win.isMaximized()
+            windowState.fullscreen = this.win.isFullScreen()
+            writeFileSync(windowDataPath, JSON.stringify(windowState))
 
-      this.webContents.send('tabs-resize');
-    };
+            this.win.setContentView(null)
 
-    this.win.on('maximize', resize);
-    this.win.on('restore', resize);
-    this.win.on('unmaximize', resize);
+            this.viewManager.clear()
 
-    this.win.on('close', async (event: Electron.Event) => {
-      const { object: settings } = Application.instance.settings;
+            if (Application.instance.windows.list.length === 1) {
+                Application.instance.dialogs.destroy()
+            }
 
-      if (settings.warnOnQuit && this.viewManager.views.size > 1) {
-        const answer = dialog.showMessageBoxSync(null, {
-          type: 'question',
-          title: `Quit ${app.name}?`,
-          message: `Quit ${app.name}?`,
-          detail: `You have ${this.viewManager.views.size} tabs open.`,
-          buttons: ['Close', 'Cancel'],
-        });
+            if (
+                incognito &&
+                Application.instance.windows.list.filter((x) => x.incognito)
+                    .length === 1
+            ) {
+                Application.instance.sessions.clearCache("incognito")
+                Application.instance.sessions.unloadIncognitoExtensions()
+            }
 
-        if (answer === 1) {
-          event.preventDefault();
-          return;
-        }
-      }
+            Application.instance.windows.list =
+                Application.instance.windows.list.filter(
+                    (x) => x.win.id !== this.win.id
+                )
 
-      // Save current window state to a file.
-      windowState.maximized = this.win.isMaximized();
-      windowState.fullscreen = this.win.isFullScreen();
-      writeFileSync(windowDataPath, JSON.stringify(windowState));
+            Application.instance.windows.current = undefined
+        })
 
-      this.win.setContentView(null);
+        this.webContents.openDevTools({ mode: "detach" })
+        ;(async () => {
+            if (process.env.NODE_ENV === "development") {
+                this.webContents.openDevTools({ mode: "detach" })
+                await this.win.loadURL("http://localhost:4444/app.html")
+            } else {
+                await this.win.loadURL(
+                    join("file://", app.getAppPath(), "build/app.html")
+                )
+            }
+        })()
 
-      this.viewManager.clear();
+        this.win.on("enter-full-screen", async () => {
+            this.send("fullscreen", true)
+            await this.viewManager.fixBounds()
+        })
 
-      if (Application.instance.windows.list.length === 1) {
-        Application.instance.dialogs.destroy();
-      }
+        this.win.on("leave-full-screen", async () => {
+            this.send("fullscreen", false)
+            await this.viewManager.fixBounds()
+        })
 
-      if (
-        incognito &&
-        Application.instance.windows.list.filter((x) => x.incognito).length ===
-          1
-      ) {
-        Application.instance.sessions.clearCache('incognito');
-        Application.instance.sessions.unloadIncognitoExtensions();
-      }
+        this.win.on("enter-html-full-screen", () => {
+            this.viewManager.fullscreen = true
+            this.send("html-fullscreen", true)
+        })
 
-      Application.instance.windows.list =
-        Application.instance.windows.list.filter(
-          (x) => x.win.id !== this.win.id,
-        );
+        this.win.on("leave-html-full-screen", () => {
+            this.viewManager.fullscreen = false
+            this.send("html-fullscreen", false)
+        })
 
-      Application.instance.windows.current = undefined;
-    });
+        //    TODO:
+        //    this.win.on('scroll-touch-begin', () => {
+        //      this.send('scroll-touch-begin');
+        //    });
 
-    // this.webContents.openDevTools({ mode: 'detach' });
+        //    TODO:
+        //    this.win.on('scroll-touch-end', () => {
+        //      this.viewManager.selected.send('scroll-touch-end');
+        //      this.send('scroll-touch-end');
+        //    });
 
-    (async () => {
-      if (process.env.NODE_ENV === 'development') {
-        this.webContents.openDevTools({ mode: 'detach' });
-        await this.win.loadURL('http://localhost:4444/app.html');
-      } else {
-        await this.win.loadURL(
-          join('file://', app.getAppPath(), 'build/app.html'),
-        );
-      }
-    })();
+        this.win.on("focus", () => {
+            Application.instance.windows.current = this
+        })
+    }
 
-    this.win.on('enter-full-screen', async () => {
-      this.send('fullscreen', true);
-      await this.viewManager.fixBounds();
-    });
+    public get id() {
+        return this.win.id
+    }
 
-    this.win.on('leave-full-screen', async () => {
-      this.send('fullscreen', false);
-      await this.viewManager.fixBounds();
-    });
+    public get webContents() {
+        return this.win.webContents
+    }
 
-    this.win.on('enter-html-full-screen', () => {
-      this.viewManager.fullscreen = true;
-      this.send('html-fullscreen', true);
-    });
+    public fixDragging() {
+        const bounds = this.win.getBounds()
+        this.win.setBounds({
+            height: bounds.height + 1
+        })
+        this.win.setBounds(bounds)
+    }
 
-    this.win.on('leave-html-full-screen', () => {
-      this.viewManager.fullscreen = false;
-      this.send('html-fullscreen', false);
-    });
+    public send(channel: string, ...args: any[]) {
+        this.webContents.send(channel, ...args)
+    }
 
-//    TODO:
-//    this.win.on('scroll-touch-begin', () => {
-//      this.send('scroll-touch-begin');
-//    });
+    public updateTitle() {
+        const { selected } = this.viewManager
+        if (!selected) return
 
-//    TODO:
-//    this.win.on('scroll-touch-end', () => {
-//      this.viewManager.selected.send('scroll-touch-end');
-//      this.send('scroll-touch-end');
-//    });
-
-    this.win.on('focus', () => {
-      Application.instance.windows.current = this;
-    });
-  }
-
-  public get id() {
-    return this.win.id;
-  }
-
-  public get webContents() {
-    return this.win.webContents;
-  }
-
-  public fixDragging() {
-    const bounds = this.win.getBounds();
-    this.win.setBounds({
-      height: bounds.height + 1,
-    });
-    this.win.setBounds(bounds);
-  }
-
-  public send(channel: string, ...args: any[]) {
-    this.webContents.send(channel, ...args);
-  }
-
-  public updateTitle() {
-    const { selected } = this.viewManager;
-    if (!selected) return;
-
-    this.win.setTitle(
-      selected.title.trim() === ''
-        ? app.name
-        : `${selected.title} - ${app.name}`,
-    );
-  }
+        this.win.setTitle(
+            selected.title.trim() === ""
+                ? app.name
+                : `${selected.title} - ${app.name}`
+        )
+    }
 }
